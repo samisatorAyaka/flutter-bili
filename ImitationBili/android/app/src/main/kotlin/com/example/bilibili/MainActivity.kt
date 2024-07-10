807d8228cfd1db251166a33d9efcc452bb6a8089
samisatorAyaka <2451829389@qq.com>
2024-07-10 15:16:31 +0800

    2
    第二次上传

--- ImitationBili/android/app/src/main/kotlin/com/example/bilibili/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bilibili
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/flutterbilibili/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterbilibili
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
